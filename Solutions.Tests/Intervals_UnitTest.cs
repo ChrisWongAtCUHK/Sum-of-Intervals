544740d761d09461ae56f6515a963a0ccc513ebb
Chris Wong <wc38.wong@gmail.com>
2020-04-26 16:59:59 +0800

    Overlapping intervals

--- Solutions.Tests/Intervals_UnitTest.cs
+++ Solutions.Tests/Intervals_UnitTest.cs
@@ -6,15 +6,13 @@ namespace Solutions.Tests
 {
     public class IntervalsTests
     {
+        List<(int, int)[]> noOverlaps = new List<(int, int)[]>();
+        List<(int, int)[]> overlaps = new List<(int, int)[]>();
+
         [SetUp]
         public void Setup()
         {
-        }
-
-        [Test]
-        public void NonOverlappingTest()
-        {
-            var noOverlaps = new List<(int, int)[]>();
+            #region non overlapping intervals
             noOverlaps.Add(new (int, int)[1]
             {
                 (1, 5)
@@ -32,7 +30,81 @@ namespace Solutions.Tests
                 (6, 10),
                 (1, 2)
             });
+            #endregion
+        
+            #region overlapping intervals
+            overlaps.Add(new (int, int)[2]
+            {
+                (1, 5),
+                (1, 5)
+            });
+            overlaps.Add(new (int, int)[2]
+            {
+                (1, 5),
+                (5, 10)
+            });
+            overlaps.Add(new (int, int)[5]
+            {
+                (1, 4),
+                (3, 6),
+                (5, 8),
+                (7, 10),
+                (9, 12)
+            });
+            overlaps.Add(new (int, int)[5]
+            {
+                (1, 4),
+                (3, 6),
+                (5, 8),
+                (7, 10),
+                (9, 12)
+            });
+            overlaps.Add(new (int, int)[3]
+            {
+                (1, 4),
+                (7, 10),
+                (3, 5)
+            });
+            overlaps.Add(new (int, int)[4]
+            {
+                (1, 20),
+                (2, 19),
+                (5, 15),
+                (8, 12)
+            });
+            overlaps.Add(new (int, int)[5]
+            {
+                (1, 5),
+                (10, 20),
+                (1, 6),
+                (16, 19),
+                (5, 11)
+            });
+            overlaps.Add(new (int, int)[5]
+            {
+                (2, 3),
+                (2, 6),
+                (2, 4),
+                (2, 9),
+                (2, 5)
+            });
+            #endregion
+        }
 
+        [Test]
+        public void NonOverlappingTest()
+        {
+            // SumIntervals1
+            foreach(var intervals in overlaps)
+            {
+                Assert.AreEqual(Intervals.SumIntervals(intervals), Intervals.SumIntervals1(intervals));
+            } 
+        }
+
+        [Test]
+        public void OverlappingTest()
+        {
+            // SumIntervals1
             foreach(var intervals in noOverlaps)
             {
                 Assert.AreEqual(Intervals.SumIntervals(intervals), Intervals.SumIntervals1(intervals));
